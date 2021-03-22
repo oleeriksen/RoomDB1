a2c935b3e4c35a4bfadc8366589bb5191e1524ac
oleeriksen <oe@easv.dk>
2021-03-22 19:53:06 +0100

    observeOnce added

--- app/src/main/java/easv/oe/roomdb1/MainActivity.kt
+++ app/src/main/java/easv/oe/roomdb1/MainActivity.kt
@@ -2,6 +2,7 @@ package easv.oe.roomdb1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -10,9 +11,12 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import easv.oe.roomdb1.data.BEPerson
 import easv.oe.roomdb1.data.PersonRepositoryInDB
+import easv.oe.roomdb1.data.observeOnce
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "xyz"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +49,7 @@ class MainActivity : AppCompatActivity() {
                               asStrings.toTypedArray()
                                                     )
              lvNames.adapter = adapter
+            Log.d(TAG, "getAll observer notified")
         }
         mRep.getAll().observe(this, nameObserver)
 
@@ -55,11 +60,15 @@ class MainActivity : AppCompatActivity() {
         val id = cache!![pos].id
         //Toast.makeText(this, "You have clicked $name at position $pos", Toast.LENGTH_LONG).show()
         val personObserver = Observer<BEPerson> { person ->
-            Toast.makeText(this, "You have clicked ${person.name} ", Toast.LENGTH_LONG).show()
+            if (person != null)
+            {
+                Toast.makeText(this, "You have clicked ${person.name} ", Toast.LENGTH_LONG).show()
+                Log.d(TAG, "getById($id) observer notified")
+            }
 
         }
         val mRep = PersonRepositoryInDB.get()
-        mRep.getById(id).observe(this , personObserver)
+        mRep.getById(id).observeOnce(this , personObserver)
 
     }
 
